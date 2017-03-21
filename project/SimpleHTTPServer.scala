8bfef71bc4560e98a4ba000f59ee43e37f26667d
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-03-21 13:22:22 +0100

    Adds support to serve 'blog(/)' and 'documentation(/)'

--- project/SimpleHTTPServer.scala
+++ project/SimpleHTTPServer.scala
@@ -1,4 +1,4 @@
-import java.io.{ Closeable, File }
+import java.io.{Closeable, File}
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -20,9 +20,22 @@ class SimpleHTTPServer(webDirectory: File, port: Int) extends Closeable {
   // needed for the future flatMap/onComplete in the end
   private implicit val executionContext = system.dispatcher
 
-  private val route = pathPrefix("") {
-    getFromDirectory(webDirectory.getAbsolutePath) ~ getFromFile(new File(webDirectory, "index.html"))
-  }
+  private val route =
+  // TODO: this needs improving. IT'd be good to have a generic fallback to serving 'index.html' from the
+  // appropriate folder anytime the path in the request leaves the last segment (filename) blank.
+    pathPrefix("documentation") {
+      pathEndOrSingleSlash {
+        getFromFile(new File(new File(webDirectory, "documentation"), "index.html"))
+      }
+    } ~
+      pathPrefix("blog") {
+        pathEndOrSingleSlash {
+          getFromFile(new File(new File(webDirectory, "blog"), "index.html"))
+        }
+      } ~
+      getFromDirectory(webDirectory.getAbsolutePath) ~
+      getFromFile(new File(webDirectory, "index.html"))
+
 
   val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(route, "localhost", port)
 
