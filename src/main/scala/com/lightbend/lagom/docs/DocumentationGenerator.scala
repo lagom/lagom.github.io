8a43be0de325f11f061e8e11b6a189d784c1dc4a
Dale Wijnand <dale.wijnand@gmail.com>
2019-08-30 09:51:57 +0100

    Update currentLagomVersion to 1.5.2

--- src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
+++ src/main/scala/com/lightbend/lagom/docs/DocumentationGenerator.scala
@@ -24,7 +24,7 @@ object DocumentationGenerator extends App {
    */
   // Current documentation version
   val currentDocsVersion = "1.5.x"
-  val currentLagomVersion = "1.5.1"
+  val currentLagomVersion = "1.5.2"
 
   // This impacts what gets displayed on the main documentation index.
   val stableVersions = Seq(
